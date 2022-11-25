c6e5fc93b3663f334aaf1aa07988d5c056bdfa60
Lmarl Saria <lmarlsaria21@gmail.com>
2022-11-25 17:03:01 +0800

    update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import todoapp from "../assets/todoapp.png";
+import bmical from "../assets/bmical.png";
 
 const Portfolio = () => {
   const portfolios = [
@@ -7,6 +8,15 @@ const Portfolio = () => {
       id: 1,
       src: todoapp,
       title: "Todo App",
+      demo: "https://react-todo-app-nine-zeta.vercel.app/",
+      code: "https://github.com/lcsaria/react-todo-app",
+    },
+    {
+      id: 2,
+      src: bmical,
+      title: "BMI Calculator",
+      demo: "https://bmi-calculator-lcs.vercel.app/",
+      code: "https://github.com/lcsaria/bmi-calculator-lcs",
     },
   ];
 
@@ -24,7 +34,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-10 sm:px-0">
-          {portfolios.map(({ id, src, title }) => (
+          {portfolios.map(({ id, src, title, demo, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -34,13 +44,13 @@ const Portfolio = () => {
               <div className="mt-3 flex flex-col items-center">{title}</div>
               <div className="flex items-center justify-center">
                 <a
-                  href="https://react-todo-app-nine-zeta.vercel.app/"
+                  href={demo}
                   className="appearance-none w-1/2 display-none px-6 py-2 m-4 duration-200 hover:scale-105 hover:font-bold hover:font-xl hover:text-secondary"
                 >
                   Demo
                 </a>
                 <a
-                  href="https://github.com/lcsaria/react-todo-app"
+                  href={code}
                   className="appearance-none w-1/2 display-none px-6 py-2 m-4 duration-200 hover:scale-105 hover:font-bold hover:font-xl hover:text-secondary"
                 >
                   Code
